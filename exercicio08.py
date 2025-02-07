4232d87c7a2c694768539fd7a9794c12fbbf1a60
VitorGulicz <vdmgulicz@gmail.com>
2025-02-07 19:58:25 -0300

    atividade 8

--- exercicio08.py
+++ exercicio08.py
@@ -1,4 +1,4 @@
 #Escreva um programa que peça ao usuario para inserir dois numeros inteiro, some-os e exiba a mensagem: "O total é:[soma dos numeros]"
 a=int(input("Insira o primeiro valor"))
 b=int(input("Insira o segundo valor"))
-print(a+b)
+print("o total é", a+b)
